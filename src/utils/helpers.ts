26f6fc95207210bf4346c114e35a9d44cba11824
matias ramos <matiasramos261@gmail.com>
2024-12-24 17:39:09 -0300

    remove function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Item, BuyProduct, Movement, Product, SaleProduct } from "./types";
+import { Item, BuyProduct, Product, SaleProduct } from "./types";
 
 export function getProductSalePrice(product: Product): number {
     if (product.sale_price && +product.sale_price > 0) return +product.sale_price;
@@ -10,17 +9,6 @@ export function getItemSalePrice(item: Item): number {
     const { product_sale_price, product_buy_price, product_earn } = item;
     if (product_sale_price && +product_sale_price > 0) return +product_sale_price;
     return parseFloat((+product_buy_price + ((+product_buy_price / 100) * +product_earn)).toFixed(2));
-}
-
-export function getStock(saleProducts: SaleProduct[], buyProducts: BuyProduct[], movements: Movement[]): number {
-    const salesAmount = saleProducts.reduce((acc, p) => acc + +p.amount, 0);
-    const purchasesAmount = buyProducts.reduce((acc, p) => acc + +p.amount, 0);
-    const movementsAmount = movements.reduce((acc, m) => {
-        if (m.type === 'INGRESO') return acc + +m.amount;
-        if (m.type === 'EGRESO') return acc - +m.amount;
-        return acc;
-    }, 0);
-    return parseFloat((purchasesAmount + movementsAmount - salesAmount).toFixed(2))
 }
 
 export function getSaleTotal(items: Item[]): number {
